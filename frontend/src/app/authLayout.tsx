95d6def5bd279f70b695cb30991bd23a4afafea1
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-12-15 09:10:46 +0300

    prevent rendering before redirecting

--- frontend/src/app/authLayout.tsx
+++ frontend/src/app/authLayout.tsx
@@ -2,25 +2,31 @@
 
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const router = useRouter();
   const { status } = useSession();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (status === "loading") return;
-    if (status === "authenticated" && pathname === "/auth") router.push("/");
-    if (
-      status === "unauthenticated" && pathname === "/dashboard") router.push("/auth");
+    if (status === "loading") {
+      setIsLoading(true);
+    } else if (status === "authenticated" && pathname === "/auth") {
+      router.push("/");
+    } else if (status === "unauthenticated" && pathname === "/dashboard") {
+      router.push("/auth");
+    } else {
+      setIsLoading(false);
+    }
   }, [status, pathname, router]);
 
-  if (status === "loading") {
+  if (isLoading) {
     return <p>Loading...</p>;
+  } else {
+    return <>{children}</>;
   }
-
-  return <>{children}</>
-}
+};
 
 export default AuthLayout;
