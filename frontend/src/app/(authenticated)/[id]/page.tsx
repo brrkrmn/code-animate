e11ac2813cd43f7fba9fd7f735c237a1797d246a
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-12-18 08:47:26 +0300

    use query to delete scene

--- frontend/src/app/(authenticated)/[id]/page.tsx
+++ frontend/src/app/(authenticated)/[id]/page.tsx
@@ -2,14 +2,27 @@
 
 import Loading from "@/components/Loading/Loading";
 import sceneService from "@/services/scene/scene";
-import { useQuery } from "@tanstack/react-query";
-import { usePathname } from "next/navigation";
+import { Button } from "@nextui-org/react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { usePathname, useRouter } from "next/navigation";
 
 const Scene = () => {
+  const router = useRouter();
   const id = usePathname();
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["userScene"],
     queryFn: () => sceneService.getScene(id),
+  });
+  const mutation = useMutation({
+    mutationFn: () => {
+      return sceneService.deleteScene(id);
+    },
+    onSuccess: () => {
+      router.push("/dashboard");
+    },
+    onError: (error) => {
+      console.log("Error: ", error.message);
+    },
   });
 
   if (isPending) return <Loading />;
@@ -19,6 +32,7 @@ const Scene = () => {
     <div>
       <div>User: {data.user.name}</div>
       <div>Title: {data.title}</div>
+      <Button onPress={() => mutation.mutate()}>DELETE THIS SCENE</Button>
     </div>
   );
 };
