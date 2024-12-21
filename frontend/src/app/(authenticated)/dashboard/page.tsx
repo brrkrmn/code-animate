e04c66557c56f90e5ca075fb5be7c983d8335987
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-12-21 09:38:12 +0300

    create scene with id

--- frontend/src/app/(authenticated)/dashboard/page.tsx
+++ frontend/src/app/(authenticated)/dashboard/page.tsx
@@ -4,9 +4,13 @@ import { useCreateScene, useGetScenes } from "@/hooks/useScene";
 import { Button } from "@nextui-org/react";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { v4 as uuidv4 } from "uuid";
 
-const mock = {
-  title: "new scene of hotmail",
+const id = uuidv4();
+
+const defaultScene = {
+  id: id,
+  title: "Untitled",
   public: false,
   steps: [
     {
@@ -23,8 +27,12 @@ const mock = {
 };
 
 const Dashboard = () => {
-  const createSceneMutation = useCreateScene(mock);
+  const createMutation = useCreateScene(defaultScene);
   const { data: scenes } = useGetScenes();
+
+  const onCreate = () => {
+    createMutation.mutate();
+  };
 
   return (
     <div className="w-full h-full min-h-screen flex flex-col items-start justify-start gap-6 py-6">
@@ -43,9 +51,7 @@ const Dashboard = () => {
       <div className="flex items-center justify-center gap-2">
         <Button>Sort</Button>
         <Button>Filter</Button>
-        <Button onPress={() => createSceneMutation.mutate()}>
-          Create Scene
-        </Button>
+        <Button onPress={onCreate}>Create Scene</Button>
       </div>
       <div className="flex flex-col items-center justify-start">
         {scenes?.map((scene) => (
