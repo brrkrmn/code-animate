725dc4fa97cef80e4bef73c9ccc2516c60dfd2b1
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-01-05 13:16:23 +0300

    style dashboard buttons

--- frontend/src/app/(authenticated)/dashboard/page.tsx
+++ frontend/src/app/(authenticated)/dashboard/page.tsx
@@ -4,8 +4,8 @@ import Filter from "@/components/Filter/Filter";
 import Sort from "@/components/Sort/Sort";
 import { useScenesContext } from "@/context/scenes";
 import { useCreateScene } from "@/hooks/useScene";
-import { Button } from "@nextui-org/react";
 import Link from "next/link";
+import { FaPlus } from "react-icons/fa6";
 import { v4 as uuidv4 } from "uuid";
 
 const id = uuidv4();
@@ -38,10 +38,18 @@ const Dashboard = () => {
 
   return (
     <div className="w-full h-full min-h-screen flex flex-col items-start justify-start gap-6 py-6">
-      <div className="flex items-center justify-center gap-2">
-        <Sort />
-        <Filter />
-        <Button onPress={onCreate}>Create Scene</Button>
+      <div className="flex items-center justify-center gap-2 flex-wrap">
+        <div className="flex items-center justify-center gap-2">
+          <Sort />
+          <Filter />
+        </div>
+        <button
+          onClick={onCreate}
+          className="flex items-center justify-center gap-2 w-fit px-5 py-1 rounded-full border-small border-divider bg-content1 text-foreground-100 hover:text-foreground-50 shadow-large transition"
+        >
+          <FaPlus className="w-4 h-4" />
+          <p>Create Scene</p>
+        </button>
       </div>
       <div className="flex flex-col items-center justify-start">
         {filteredScenes?.map((scene) => (
