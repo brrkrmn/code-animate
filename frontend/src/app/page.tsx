8c82ebef64784c3f04f0a50298ede3191c6f875f
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-01-07 11:07:00 +0300

    style landing title

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,33 @@
 "use client"
 
-import { Button } from "@nextui-org/react";
-import Link from "next/link";
+// import { Button } from "@nextui-org/react";
+// import Link from "next/link";
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
   return (
     <div className="min-h-screen py-2 h-full flex items-center justify-center">
-      <Button radius="full" variant="bordered" size="lg" color="default">
-        <Link href="auth">Login</Link>
-      </Button>
+      <TypeAnimation
+        wrapper="span"
+        repeat={0}
+        cursor={true}
+        preRenderFirstString={true}
+        speed={30}
+        deletionSpeed={10}
+        sequence={[
+          "Code",
+          1000,
+          "Code Animation",
+          1000,
+          "Code Animat",
+          200,
+          "Code Animate",
+          1000,
+          "Codeanimate",
+          1000,
+          "Codymate",
+        ]}
+      />
     </div>
   );
 };
