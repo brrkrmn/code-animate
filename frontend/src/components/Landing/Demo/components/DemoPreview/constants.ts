dc8822e64df44cec535bd7d48b80fb16653de10c
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-02-05 15:11:59 +0300

    update demo preview

--- frontend/src/components/Landing/Demo/components/DemoPreview/constants.ts
+++ frontend/src/components/Landing/Demo/components/DemoPreview/constants.ts
@@ -9,18 +9,155 @@ export const demoScene = {
   steps: [
     {
       id: "9d8b4b0d-e3a5-47dc-9fd6-1e20f49cc237",
-      content:
-        '//This is an example preview\nconst { scrollYProgress } = useScroll({\n  target: sectionRef,\n  offset: ["start end", "end start"],\n});\n\nconst background = useTransform(\n  scrollYProgress,\n  [0, 0.5, 1],\n  [\n    "radial-gradient(circle, rgba(24,67,96,1) 0%, rgba(5,6,15,1) 0%)",\n    "radial-gradient(circle, rgba(24,67,96,1) 0%, rgba(5,6,15,1) 60%)",\n    "radial-gradient(circle, rgba(24,67,96,1) 0%, rgba(5,6,15,1) 0%)",\n  ]\n);',
+      content: `if (lineDiffSet[1].added) {
+  changedLine.split("").map((char, index) => {
+    transactions.push({
+      from: lineDiffSet[0].value.length - 1 + index,
+      insert: char,
+    });
+  });
+} else if (lineDiffSet[1].removed) {
+  for (let i = changedLine.length; i >= 0; i--) {
+    transactions.push({
+      from: //the last position
+      to: //to minus 1
+      insert: //empty string,
+    });
+  }
+}`,
     },
     {
       id: "cdd6babd-ec2d-4c47-9742-8804b9cac248",
-      content:
-        '//This is an example preview\nconst { scrollYProgress } = useScroll({\n  target: sectionRef,\n  offset: ["start end", "end start"],\n});\n\nconst background = useTransform( //this line animates the background color as you scroll\n  scrollYProgress,\n  [0, 0.5, 1],\n  [\n    "radial-gradient(circle, rgba(24,67,96,1) 0%, rgba(5,6,15,1) 0%)",\n    "radial-gradient(circle, rgba(24,67,96,1) 0%, rgba(5,6,15,1) 60%)",\n    "radial-gradient(circle, rgba(24,67,96,1) 0%, rgba(5,6,15,1) 0%)",\n  ]\n);',
+      content: `if (lineDiffSet[1].added) {
+  changedLine.split("").map((char, index) => {
+    transactions.push({
+      from: lineDiffSet[0].value.length - 1 + index,
+      insert: char,
+    });
+  });
+} else if (lineDiffSet[1].removed) {
+  for (let i = changedLine.length - 1; i >= 0; i--) {
+    transactions.push({
+      from: //the last position
+      to: //to minus 1
+      insert: //empty string,
+    });
+  }
+}`,
+    },
+    {
+      id: "d771102d-023e-4286-bb40-9575fe2c86df122",
+      content: `if (lineDiffSet[1].added) {
+  changedLine.split("").map((char, index) => {
+    transactions.push({
+      from: lineDiffSet[0].value.length - 1 + index,
+      insert: char,
+    });
+  });
+} else if (lineDiffSet[1].removed) {
+  for (let i = changedLine.length - 1; i >= 0; i--) {
+    transactions.push({
+      from:
+      to: //to minus 1
+      insert: //empty string,
+    });
+  }
+}`,
     },
     {
       id: "d771102d-023e-4286-bb40-9575fe2c86df",
-      content:
-        '//This is an example preview\nconst { scrollYProgress } = useScroll({\n  target: sectionRef,\n  offset: ["start end", "end start"],\n});\n\nconst background = useTransform( //this line animates the background color as you scroll\n  scrollYProgress,\n  [0, 0.5, 1],\n  [\n    "radial-gradient(circle, rgba(24,67,96,1) 0%, rgba(5,6,15,1) 0%)",\n    "radial-gradient(circle, rgba(24,67,96,1) 0%, rgba(5,6,15,1) 60%)",\n    "radial-gradient(circle, rgba(24,67,96,1) 0%, rgba(5,6,15,1) 0%)",\n  ]\n); //now backgroud can be used inline in the motion.div',
+      content: `if (lineDiffSet[1].added) {
+  changedLine.split("").map((char, index) => {
+    transactions.push({
+      from: lineDiffSet[0].value.length - 1 + index,
+      insert: char,
+    });
+  });
+} else if (lineDiffSet[1].removed) {
+  for (let i = changedLine.length - 1; i >= 0; i--) {
+    transactions.push({
+      from: lineDiffSet[0].value.length + i - 1,
+      to: //to minus 1
+      insert: //empty string,
+    });
+  }
+}`,
+    },
+    {
+      id: "d771102d-023e-4286-bb40-9575f232e2c86df1",
+      content: `if (lineDiffSet[1].added) {
+  changedLine.split("").map((char, index) => {
+    transactions.push({
+      from: lineDiffSet[0].value.length - 1 + index,
+      insert: char,
+    });
+  });
+} else if (lineDiffSet[1].removed) {
+  for (let i = changedLine.length - 1; i >= 0; i--) {
+    transactions.push({
+      from: lineDiffSet[0].value.length + i - 1,
+      to:
+      insert: //empty string,
+    });
+  }
+}`,
+    },
+    {
+      id: "d771102d-023e-4286-bb40-9575fe2c86df1",
+      content: `if (lineDiffSet[1].added) {
+  changedLine.split("").map((char, index) => {
+    transactions.push({
+      from: lineDiffSet[0].value.length - 1 + index,
+      insert: char,
+    });
+  });
+} else if (lineDiffSet[1].removed) {
+  for (let i = changedLine.length - 1; i >= 0; i--) {
+    transactions.push({
+      from: lineDiffSet[0].value.length + i - 1,
+      to: lineDiffSet[0].value.length + i,
+      insert: //empty string,
+    });
+  }
+}`,
+    },
+    {
+      id: "d771102d-023e-4286-bb40-9575fe2c86df2",
+      content: `if (lineDiffSet[1].added) {
+  changedLine.split("").map((char, index) => {
+    transactions.push({
+      from: lineDiffSet[0].value.length - 1 + index,
+      insert: char,
+    });
+  });
+} else if (lineDiffSet[1].removed) {
+  for (let i = changedLine.length - 1; i >= 0; i--) {
+    transactions.push({
+      from: lineDiffSet[0].value.length + i - 1,
+      to: lineDiffSet[0].value.length + i,
+      insert:
+    });
+  }
+}`,
+    },
+    {
+      id: "d771102d-023e-4286-bb40-9575fe2c86df2",
+      content: `if (lineDiffSet[1].added) {
+  changedLine.split("").map((char, index) => {
+    transactions.push({
+      from: lineDiffSet[0].value.length - 1 + index,
+      insert: char,
+    });
+  });
+} else if (lineDiffSet[1].removed) {
+  for (let i = changedLine.length - 1; i >= 0; i--) {
+    transactions.push({
+      from: lineDiffSet[0].value.length + i - 1,
+      to: lineDiffSet[0].value.length + i,
+      insert: "",
+    });
+  }
+}`,
     },
   ],
 };
