ec6e7f5d416216b1dfa447ea8d99e44bad96ec7d
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-02-14 11:15:37 +0300

    show success toast on tryout scene creation

--- frontend/src/app/(unauthenticated)/tryout/[id]/layout.tsx
+++ frontend/src/app/(unauthenticated)/tryout/[id]/layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import SceneProvider from "@/context/scene/sceneProvider";
+import { useToastContext } from "@/context/toast";
+import { toasts } from "@/context/toast/toast.constants";
 import { Scene } from "@/services/scene/scene.types";
 import getSceneFromLs from "@/utils/localStorage/getSceneFromLs/getSceneFromLs";
 import removeOrEditSceneInLs from "@/utils/localStorage/removeOrEditSceneInLs/removeOrEditSceneInLs";
@@ -10,6 +12,7 @@ const TryoutLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const params = useParams();
   const id = params.id as string;
+  const { toast } = useToastContext();
 
   const scene = getSceneFromLs(id);
 
@@ -20,6 +23,7 @@ const TryoutLayout = ({ children }: { children: React.ReactNode }) => {
 
   const onSave = (editedScene: Scene) => {
     removeOrEditSceneInLs(id, editedScene);
+    toast(toasts.createScene.success);
   };
 
   if (!scene) return null;
